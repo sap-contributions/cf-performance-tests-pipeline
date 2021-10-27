2a030ad676819a253726dadb3109b8fb7435c375
Jochen Ehret <jochen.ehret@sap.com>
2021-10-27 09:02:42 +0200

    Add AWS sg for ssh database tunnel
    * must be able to reach Postgres db from jumpbox

---
+++ bbl-patches/db-inbound-sg.tf
@@ -0,0 +1,9 @@
+# required for ssh tunnel to postgres db over jumpbox
+resource "aws_security_group_rule" "internal_security_group_rule_ssh" {
+  security_group_id        = "${aws_security_group.internal_security_group.id}"
+  type                     = "ingress"
+  protocol                 = "TCP"
+  from_port                = 0
+  to_port                  = 5524
+  source_security_group_id = "${aws_security_group.jumpbox.id}"
+}
