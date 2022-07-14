4aef1e91aa53fdbf5f58000cdc4bf1b7de25045d
Jochen Ehret <jochen.ehret@sap.com>
2022-07-14 08:12:14 +0200

    CFP-1806 Pipeline config param for db type
    * adapt tunnel for MySQL
    * add security group to enable tunnel over jumpbox

--- bbl-patches/terraform/db-inbound-sg.tf
+++ bbl-patches/terraform/db-inbound-sg.tf
@@ -7,3 +7,13 @@ resource "aws_security_group_rule" "internal_security_group_rule_db_tunnel" {
   to_port                  = 3306
   source_security_group_id = "${aws_security_group.jumpbox.id}"
 }
+
+# required for ssh tunnel to MySQL db over jumpbox
+resource "aws_security_group_rule" "internal_security_group_rule_mysql_db_tunnel" {
+  security_group_id        = "${aws_security_group.internal_security_group.id}"
+  type                     = "ingress"
+  protocol                 = "TCP"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = "${aws_security_group.jumpbox.id}"
+}
