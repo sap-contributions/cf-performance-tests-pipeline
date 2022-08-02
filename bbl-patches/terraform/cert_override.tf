7a5e7b22b4b9ceea11e5a38902b73a20cce5cbfb
Will Gant <william.gant@sap.com>
2022-08-02 17:13:05 +0100

    Trick bbl into making our cert for us

--- bbl-patches/terraform/cert_override.tf
+++ bbl-patches/terraform/cert_override.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    acme = {
-      source = "vancluever/acme"
-      version = "~> 2.0"
-    }
-  }
-}
-
 provider "acme" {
   server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
   #TODO switch to production URL
@@ -18,12 +9,12 @@ resource "tls_private_key" "private_key" {
 }
 
 resource "acme_registration" "reg" {
-  account_key_pem = tls_private_key.private_key.private_key_pem
+  account_key_pem = "${tls_private_key.private_key.private_key_pem}"
   email_address   = "performance-tests@cloudfoundry.com"
 }
 
 resource "acme_certificate" "certificate" {
-  account_key_pem           = acme_registration.reg.account_key_pem
+  account_key_pem           = "${acme_registration.reg.account_key_pem}"
   common_name               = "*.cf.${var.domain}"
   subject_alternative_names = [
     "*.cfapps.cf.${var.domain}", 
@@ -31,19 +22,22 @@ resource "acme_certificate" "certificate" {
     "*.uaa.cf.${var.domain}"
     ]
 
-  recursive_nameservers = ["8.8.8.8:53"]
-
   dns_challenge {
     provider = "route53"
   }
 }
 
-output "private_key" {
-    value = acme_certificate.certificate.private_key_pem
-    sensitive = true
+resource "aws_iam_server_certificate" "lb_cert" {
+  name_prefix = "${var.short_env_id}"
+
+  certificate_body = "${acme_certificate.certificate.certificate_pem}"
+  private_key      = "${acme_certificate.certificate.private_key_pem}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "certificate" {
-    value = acme_certificate.certificate.certificate_pem
-    sensitive = true
+variable "domain" {
+  type = "string"
 }
---
+++ bbl-patches/terraform/lb_override.tf
@@ -0,0 +1,42 @@
+resource "aws_elb" "cf_router_lb" {
+  name                      = "${var.short_env_id}-cf-router-lb"
+  cross_zone_load_balancing = true
+
+  health_check {
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    interval            = 12
+    target              = "TCP:80"
+    timeout             = 2
+  }
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port      = 80
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "${aws_iam_server_certificate.lb_cert.arn}"
+  }
+
+  listener {
+    instance_port      = 80
+    instance_protocol  = "tcp"
+    lb_port            = 4443
+    lb_protocol        = "ssl"
+    ssl_certificate_id = "${aws_iam_server_certificate.lb_cert.arn}"
+  }
+
+  security_groups = ["${aws_security_group.cf_router_lb_security_group.id}"]
+  subnets         = ["${aws_subnet.lb_subnets.*.id}"]
+
+  tags = {
+    Name = "${var.env_id}"
+  }
+}
