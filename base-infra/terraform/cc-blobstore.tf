32c2b5e407ed64bed32ea9dc9fe7b6c05cd32fa1
Will Gant <william.gant@sap.com>
2022-08-04 18:29:32 +0100

    Cut duplicate names in terraform

--- base-infra/terraform/cc-blobstore.tf
+++ base-infra/terraform/cc-blobstore.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "cc-blobstore-packages" {
   bucket = "${var.env_name}-perf-tests-packages"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "packages" {
   bucket = aws_s3_bucket.cc-blobstore-packages.bucket
 
   rule {
@@ -16,8 +16,8 @@ resource "aws_s3_bucket" "cc-blobstore-buildpacks" {
   bucket = "${var.env_name}-perf-tests-buildpacks"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.cc-blobstore-packages.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "buildpacks" {
+  bucket = aws_s3_bucket.cc-blobstore-buildpacks.bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -30,8 +30,8 @@ resource "aws_s3_bucket" "cc-blobstore-droplets" {
   bucket = "${var.env_name}-perf-tests-dropletss"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.cc-blobstore-packages.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "droplets" {
+  bucket = aws_s3_bucket.cc-blobstore-droplets.bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -44,8 +44,8 @@ resource "aws_s3_bucket" "cc-blobstore-resources" {
   bucket = "${var.env_name}-perf-tests-resources"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.cc-blobstore-packages.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "resources" {
+  bucket = aws_s3_bucket.cc-blobstore-resources.bucket
 
   rule {
     apply_server_side_encryption_by_default {
