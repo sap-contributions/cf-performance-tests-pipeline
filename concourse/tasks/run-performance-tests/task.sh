e9d4a62ec5f0a8e86d93f864220d5b8274b1e0ec
Will Gant <william.gant@sap.com>
2022-08-04 14:09:35 +0100

    Refactor run-performance-tests

--- concourse/tasks/run-performance-tests/task.sh
+++ concourse/tasks/run-performance-tests/task.sh
@@ -2,108 +2,101 @@
 
 set -eu
 
+task_root="$(pwd)"
+cf_perf_tests_pipeline_repo="${task_root}/perf-test-repo"
+cf_perf_tests_repo="${task_root}/cf-performance-tests"
+cf_deployment_repo="${task_root}/cf_deployment"
+bbl_state="${task_root}/bbl-state"
+performance_test_results="${task_root}/performance-test-results"
+
 echo -e "\nGetting test landscape configuration from bbl state..."
-pushd "bbl-state/state" > /dev/null
+pushd "$bbl_state" >/dev/null
   eval "$(bbl print-env)"
-  BOSH_DIRECTOR_NAME="$(<bbl-state.json jq -r .bosh.directorName)"
-  CF_ADMIN_PASSWORD="$(credhub get -n "/$BOSH_DIRECTOR_NAME/cf/cf_admin_password" -j | jq -r .value)"
-  CF_DOMAIN="$(<bbl-state.json jq -r .lb.domain)"
-  JUMPBOX_URL="$(<./vars/jumpbox-vars-file.yml yq -r .external_ip)"
-  JUMPBOX_PRIVATE_KEY="$(<./vars/jumpbox-vars-store.yml yq -r .jumpbox_ssh.private_key)"
-  DATABASE_IP="$(bosh -d cf vms --json | jq -r '.Tables[0].Rows[] | select(.instance | startswith("database/")) | .ips')"
-  CCDB_PASSWORD="$(credhub get -n "/$BOSH_DIRECTOR_NAME/cf/cc_database_password" -j | jq -r .value)"
-  UAADB_PASSWORD="$(credhub get -n "/$BOSH_DIRECTOR_NAME/cf/uaa_database_password" -j | jq -r .value)"
-popd > /dev/null
+  bosh_director_name="$(<bbl-state.json jq --raw-output '.bosh.directorName')"
+  cf_admin_password="$(credhub get --name "/${bosh_director_name}/cf/cf_admin_password" --output-json | jq --raw-output '.value')"
+  cf_domain="$(<bbl-state.json jq --raw-output '.lb.domain')"
+  jumpbox_url="$(<./vars/jumpbox-vars-file.yml yq --raw-output '.external_ip')"
+  jumpbox_private_key="$(<./vars/jumpbox-vars-store.yml yq --raw-output '.jumpbox_ssh.private_key')"
+  database_ip="$(bosh -d cf vms --json | jq --raw-output '.Tables[0].Rows[] | select(.instance | startswith("database/")) | .ips')"
+  ccdb_password="$(credhub get --name "/${bosh_director_name}/cf/cc_database_password" --output-json | jq --raw-output '.value')"
+  uaadb_password="$(credhub get --name "/${bosh_director_name}/cf/uaa_database_password" --output-json | jq --raw-output '.value')"
+popd >/dev/null
 
 echo -e "\nGetting cf-deployment version..."
-# TODO if possible we could also get the cloud_controller_ng commit
-CF_DEPLOYMENT_VERSION="$(<./cf-deployment/cf-deployment.yml yq '.manifest_version' -r)"
-CAPI_VERSION="$(<./cf-deployment/cf-deployment.yml grep -A 1 capi | grep version | cut -d ':' -f2 | sed 's/ //g')"
+cf_deployment_version="$(<"${cf_deployment_repo}"/cf-deployment.yml yq '.manifest_version' -r)"
+capi_version="$(<"${cf_deployment_repo}"/cf-deployment.yml grep -A 1 capi | grep version | cut -d ':' -f2 | sed 's/ //g')"
 
 echo -e "\nLogging in to CF and creating a test user..."
-cf api --skip-ssl-validation "api.$CF_DOMAIN"
-cf auth admin "$CF_ADMIN_PASSWORD"
+cf api --skip-ssl-validation "api.${cf_domain}"
+cf auth admin "$cf_admin_password"
 cf create-user perf-test-user perf-test-password
 
-if [ -z "${DATABASE_TYPE:-}" ]; then
-  echo "Task parameter 'DATABASE_TYPE' is empty. Falling back to 'postgres'."
-  DATABASE_TYPE="postgres"
-fi
-if [ "${DATABASE_TYPE}" == 'postgres' ]; then
-  DATABASE_PORT=5524
-  DATABASE_CCDB="postgres://cloud_controller:$CCDB_PASSWORD@localhost:$DATABASE_PORT/cloud_controller?sslmode=disable"
-  DATABASE_UAADB="postgres://uaa:$UAADB_PASSWORD@localhost:$DATABASE_PORT/uaa?sslmode=disable"
-elif [ "${DATABASE_TYPE}" == 'mysql' ]; then
-  DATABASE_PORT=3306
-  DATABASE_CCDB="cloud_controller:$CCDB_PASSWORD@tcp(localhost:$DATABASE_PORT)/cloud_controller?multiStatements=true"
-  DATABASE_UAADB="uaa:$UAADB_PASSWORD@tcp(localhost:$DATABASE_PORT)/uaa?multiStatements=true"
+if [ "$DATABASE_TYPE" == 'postgres' ]; then
+  database_port=5524
+  database_ccdb="postgres://cloud_controller:${ccdb_password}@localhost:${database_port}/cloud_controller?sslmode=disable"
+  database_uaadb="postgres://uaa:${uaadb_password}@localhost:${database_port}/uaa?sslmode=disable"
+elif [ "$DATABASE_TYPE" == 'mysql' ]; then
+  database_port=3306
+  database_ccdb="cloud_controller:${ccdb_password}@tcp(localhost:${database_port})/cloud_controller?multiStatements=true"
+  database_uaadb="uaa:${uaadb_password}@tcp(localhost:${database_port})/uaa?multiStatements=true"
 else
   echo "Task parameter 'DATABASE_TYPE' must be one of 'postgres' or 'mysql' (is: ${DATABASE_TYPE})."
   exit 1
 fi
 
+jumpbox_private_key_file="$(mktemp)"
+chmod 0600 "$jumpbox_private_key_file"
+cat <<EOF > "$jumpbox_private_key_file"
+$jumpbox_private_key
+EOF
+
 echo -e "\nOpening SSH tunnel to CF database..."
-echo "$JUMPBOX_PRIVATE_KEY" > ./jumpbox_private.key
-chmod 0600 ./jumpbox_private.key
-ssh -4 -N -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o "ServerAliveInterval=30" -o "ServerAliveCountMax=10" -o "IPQoS=throughput" -i ./jumpbox_private.key -L "$DATABASE_PORT":"$DATABASE_IP":"$DATABASE_PORT" jumpbox@"$JUMPBOX_URL" &
-SSH_PID=$!
+ssh -4 -N -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o "ServerAliveInterval=30" -o "ServerAliveCountMax=10" -o "IPQoS=throughput" -i "$jumpbox_private_key_file" -L "$database_port":"$database_ip":"$database_port" jumpbox@"$jumpbox_url" &
+ssh_pid=$!
 
 echo -e "\nRunning cf-performance tests..."
-if [ -z "${TEST_RESULTS_FOLDER:-}" ]; then
-  echo "Task parameter 'TEST_RESULTS_FOLDER' is empty. Check pipeline configuration."
-  exit 1
-fi
-TEST_RESULTS="$PWD/$TEST_RESULTS_FOLDER"
-pushd "./cf-performance-tests" > /dev/null
+test_results="${cf_perf_tests_pipeline_repo}/${TEST_RESULTS_FOLDER}"
+
+pushd "$cf_perf_tests_repo" >/dev/null
   cat << EOF > ./config.yml
-api: "api.$CF_DOMAIN"
+api: "api.${cf_domain}"
 skip_ssl_validation: true
-cf_deployment_version: "$CF_DEPLOYMENT_VERSION"
-capi_version: "$CAPI_VERSION"
+cf_deployment_version: "$cf_deployment_version"
+capi_version: "$capi_version"
 users:
   admin:
     username: "admin"
-    password: "$CF_ADMIN_PASSWORD"
+    password: "$cf_admin_password"
 database_type: "$DATABASE_TYPE"
-ccdb_connection: "$DATABASE_CCDB"
-uaadb_connection: "$DATABASE_UAADB"
+ccdb_connection: "$database_ccdb"
+uaadb_connection: "$database_uaadb"
 samples: 10
-results_folder: "$TEST_RESULTS"
+results_folder: "$test_results"
 EOF
   if [ -z "${TEST_SUITE_FOLDER:-}" ]; then
+    echo -e "\nRunning tests in ${TEST_SUITE_FOLDER}..."
     ginkgo ./...
   else
+    echo -e "\nRunning all tests..."
     ginkgo -r "$TEST_SUITE_FOLDER"
   fi
-popd > /dev/null
-
-echo -e "\nPushing cf-performance test results..."
-# must copy original git repository to the "repository" folder specified in the "put" resource
-cp -r "perf-test-repo/." "performance-test-results/"
+popd >/dev/null
 
 echo -e "\n Installing git lfs"
-tmpdir="$(mktemp -d git_lfs_install.XXXXXX)"
-
-cd "$tmpdir"
-curl -Lo git.tar.gz https://github.com/github/git-lfs/releases/download/v1.1.0/git-lfs-linux-386-1.1.0.tar.gz
-gunzip git.tar.gz
-tar xf git.tar
-mv git-lfs-1.1.0/git-lfs /usr/bin
-cd ..
-rm -rf "$tmpdir"
+apt-get update && apt-get -y install git-lfs
 git lfs install
 
-pushd "performance-test-results" > /dev/null
-  status="$(git status --porcelain)"
-  if [[ -n "$status" ]]; then
-    git config user.name "${GIT_COMMIT_USERNAME}"
-    git config user.email "${GIT_COMMIT_EMAIL}"
-    git add --all .
-    git commit -m "${GIT_COMMIT_MESSAGE}"
-  fi
-popd > /dev/null
+if [[ $(git -C "$cf_perf_tests_pipeline_repo" status --porcelain) ]]; then
+  echo -e "\nCommitting test results..."
+  git -C "$cf_perf_tests_pipeline_repo" config user.name "${GIT_COMMIT_USERNAME}"
+  git -C "$cf_perf_tests_pipeline_repo" config user.email "${GIT_COMMIT_EMAIL}"
+  git -C "$cf_perf_tests_pipeline_repo" add --all "$test_results"
+  git -C "$cf_perf_tests_pipeline_repo" commit -m "${GIT_COMMIT_MESSAGE}"
+fi
 
 echo "Killing background ssh tunnel..."
-kill $SSH_PID
+kill "$ssh_pid"
 
-echo "Finished."
+cp -r "${cf_perf_tests_pipeline_repo}/." "${performance_test_results}/"
+
+echo -e "\nFinished."
