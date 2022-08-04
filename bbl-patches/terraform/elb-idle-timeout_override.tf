970682f71814c03fb6e3365804fe4dfa709a0186
Will Gant <william.gant@sap.com>
2022-08-04 14:37:31 +0100

    Override elb timeout from pipeline var files

---
+++ bbl-patches/terraform/elb-idle-timeout_override.tf
@@ -0,0 +1,9 @@
+resource "aws_elb" "cf_router_lb" {
+  idle_timeout = "${var.idle_timeout}"
+}
+
+variable "idle_timeout" {
+  type        = number
+  default     = 60
+  description = "The time in seconds that a connection to the cf router load balancer is allowed to be idle"
+}
