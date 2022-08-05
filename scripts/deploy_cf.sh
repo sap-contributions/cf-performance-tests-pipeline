8d222e0777740870bb68d99c395c00e913dc39ca
Will Gant <william.gant@sap.com>
2022-08-05 09:38:58 +0100

    Remove unneeded script

--- scripts/deploy_cf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export SYSTEM_DOMAIN=cf.cfperftest.<your domain>
-bosh deploy -d cf cf-deployment/cf-deployment.yml \
-  -v system_domain=$SYSTEM_DOMAIN \
-  -o cf-deployment/operations/use-postgres.yml \
-  -o operations/use-bionic-stemcell.yml \
-  --no-redact
