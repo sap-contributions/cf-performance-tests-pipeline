5a92309f0c027a5d34c2d805aeee68315b5398f0
Will Gant <william.gant@sap.com>
2022-08-05 09:00:54 +0100

    Loosen base-infra terraform version constraints

--- base-infra/terraform/provider.tf
+++ base-infra/terraform/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     tls = {
       source  = "hashicorp/tls"
-      version = "3.1.0"
+      version = ">= 4.0.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
 }
