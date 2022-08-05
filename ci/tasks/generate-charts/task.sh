5632902a83ab087922dccd721cc305826e11c9a6
Will Gant <william.gant@sap.com>
2022-08-05 17:56:12 +0100

    Fix paths in generate-charts

--- ci/tasks/generate-charts/task.sh
+++ ci/tasks/generate-charts/task.sh
@@ -4,17 +4,17 @@ set -euo pipefail
 
 task_root="$(pwd)"
 cf_perf_tests_pipeline_repo="${task_root}/cf-performance-tests-pipeline"
-test_results_folder="${task_root}${TEST_RESULTS_FOLDER}"
+test_results_folder_absolute_path="${cf_perf_tests_pipeline_repo}/${TEST_RESULTS_FOLDER}"
 generated_charts_absolute_path="${cf_perf_tests_pipeline_repo}/${GENERATED_CHARTS_FOLDER}"
 
 echo -e "\nInstalling matplotlib..."
 pip install matplotlib
 
-mkdir -p "$generated_charts_absolute_path"
+mkdir -p "$generated_charts_absolute_path" "$test_results_folder_absolute_path"
 
 echo -e "\nGenerating charts..."
 python "${cf_perf_tests_pipeline_repo}/ci/tasks/generate-charts/generate_charts.py" \
-  --test-results "$test_results_folder" --generated-charts "$generated_charts_absolute_path"
+  --test-results "$test_results_folder_absolute_path" --generated-charts "$generated_charts_absolute_path"
 
 echo -e "\n Installing git lfs"
 apt-get update && apt-get -y install git-lfs
