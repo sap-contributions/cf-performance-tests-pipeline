87230eea4a6bfc70ef06520a321fc52cd9e78e20
Will Gant <william.gant@sap.com>
2022-08-09 13:03:01 +0100

    Share state bucket among all test environments

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,14 +7,17 @@ export AWS_PAGER=""
 
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cloudformation_template="file://${script_dir}/bootstrap.yaml"
-stack_name=cf-performance-tests-pipeline
+stack_name=cf-performance-tests-shared
 
-if ! aws cloudformation describe-stacks --stack-name "$stack_name" >/dev/null; then
-  aws cloudformation create-stack --stack-name "$stack_name" --capabilities CAPABILITY_NAMED_IAM --template-body "$cloudformation_template" --on-failure DO_NOTHING >/dev/null
+if aws cloudformation describe-stacks --stack-name "$stack_name" >/dev/null 2>/dev/null; then
+  echo "Updating stack..."
+  aws cloudformation update-stack --stack-name "$stack_name" --capabilities CAPABILITY_NAMED_IAM --template-body "$cloudformation_template"
+else
+  echo "Creating stack..."
+  aws cloudformation create-stack --stack-name "$stack_name" --capabilities CAPABILITY_NAMED_IAM --template-body "$cloudformation_template" --on-failure DO_NOTHING
+  aws cloudformation wait stack-create-complete --stack-name "$stack_name"
 fi
 
-aws cloudformation update-stack --stack-name "$stack_name" --capabilities CAPABILITY_NAMED_IAM --template-body "$cloudformation_template" || true
 aws cloudformation list-stack-resources --stack-name "$stack_name" --output yaml
-
 creds_arn="$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='PipelineUserCredsARN'].OutputValue" --output text)"
 echo -e "\n$(tput setaf 0)$(tput bold)Ensure that the AWS credentials of the created user (found at $(tput setaf 4)${creds_arn}$(tput setaf 0)$(tput bold)) are added to credhub under /concourse/cf-controlplane/aws-pipeline-user-id and /concourse/cf-controlplane/aws-pipeline-user-secret"
