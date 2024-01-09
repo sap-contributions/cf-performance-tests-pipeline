7ef10c53a032ab470fe75750e5241d2ef1caf61d
Jochen Ehret <jochen.ehret@sap.com>
2024-01-09 10:10:43 +0100

    Helper task for extracting cf-deployment and capi versions

---
+++ ci/tasks/get-artifact-versions/task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+task_root="$(pwd)"
+cf_deployment_repo="${task_root}/cf-deployment"
+cf_versions_output="${task_root}/cf-versions"
+
+echo -e "\nGetting cf-deployment and capi versions..."
+cf_deployment_version="$(<"${cf_deployment_repo}"/cf-deployment.yml yq '.manifest_version' -r)"
+capi_version="$(<"${cf_deployment_repo}"/cf-deployment.yml grep -A 1 capi | grep version | cut -d ':' -f2 | sed 's/ //g')"
+
+echo "cf_deployment_version: ${cf_deployment_version}" >> "${cf_versions_output}/cf_versions.yml"
+echo "capi_version: ${capi_version}" >> "${cf_versions_output}/cf_versions.yml"
