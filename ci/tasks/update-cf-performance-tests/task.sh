3ebeff1f14b6f85c83a799f8010d052479c40115
Jochen Ehret <jochen.ehret@sap.com>
2024-01-12 11:44:00 +0100

    Check if there are git changes in update task

--- ci/tasks/update-cf-performance-tests/task.sh
+++ ci/tasks/update-cf-performance-tests/task.sh
@@ -8,8 +8,12 @@ pushd src/cf-performance-tests >/dev/null
   commit_message="$(git log --format="%h %s" -1)"
 popd >/dev/null
 
-git add ./src/cf-performance-tests
-git config --global user.name "testname"
-git config --global user.email "test@sap.com"
-git commit -m "$commit_message"
-echo "Finished updating submodule."
+if [ -z "$(git status --porcelain=v1 2>/dev/null)" ]; then
+  echo "No submodule update to commit."
+else
+  git add ./src/cf-performance-tests
+  git config --global user.name "testname"
+  git config --global user.email "test@sap.com"
+  git commit -m "$commit_message"
+  echo "Updated submodule to $commit_commit_message"
+fi
