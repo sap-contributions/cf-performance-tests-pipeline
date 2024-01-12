a3b9168fe30e9160ad84846d28ed182dabd3f0ba
Jochen Ehret <jochen.ehret@sap.com>
2024-01-12 15:06:39 +0100

    Add bosh vendor-package commands

--- ci/tasks/create-final-bosh-release/task.sh
+++ ci/tasks/create-final-bosh-release/task.sh
@@ -3,10 +3,12 @@
 set -euo pipefail
 
 cd cf-performance-tests-release
+bosh vendor-package cf-cli-8-linux ../bosh-package-cf-cli-release
+bosh vendor-package golang-1.21-linux ../bosh-package-golang-release
 bosh create-release --final --tarball=releases/cf-performance-errand.tgz
 
 git add .
-git config --global user.name "testname"
-git config --global user.email "test@sap.com"
+git config --global user.name "$GIT_COMMIT_USERNAME"
+git config --global user.email "$GIT_COMMIT_EMAIL"
 git commit -m "Final BOSH release"
 echo "Finished creating BOSH release."
