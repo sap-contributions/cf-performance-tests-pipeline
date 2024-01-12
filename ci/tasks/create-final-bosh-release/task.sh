efb3fa84d0cee1eb66bb07da96aeda80ccb49b15
Jochen Ehret <jochen.ehret@sap.com>
2024-01-12 15:13:54 +0100

    Commit changes after vendoring packages

--- ci/tasks/create-final-bosh-release/task.sh
+++ ci/tasks/create-final-bosh-release/task.sh
@@ -3,8 +3,19 @@
 set -euo pipefail
 
 cd cf-performance-tests-release
+
 bosh vendor-package cf-cli-8-linux ../bosh-package-cf-cli-release
 bosh vendor-package golang-1.21-linux ../bosh-package-golang-release
+if [ -z "$(git status --porcelain=v1 2>/dev/null)" ]; then
+  echo "No changes in vendored packages to commit."
+else
+  git add .
+  git config --global user.name "$GIT_COMMIT_USERNAME"
+  git config --global user.email "$GIT_COMMIT_EMAIL"
+  git commit -m "Update vendored packages"
+  echo "Updated vendored packages."
+fi
+
 bosh create-release --final --tarball=releases/cf-performance-errand.tgz
 
 git add .
