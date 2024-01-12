21a71ff2fb4215526b33834af9086fac648123cd
Jochen Ehret <jochen.ehret@sap.com>
2024-01-12 11:49:29 +0100

    Fix update task

--- ci/tasks/update-cf-performance-tests/task.sh
+++ ci/tasks/update-cf-performance-tests/task.sh
@@ -12,8 +12,8 @@ if [ -z "$(git status --porcelain=v1 2>/dev/null)" ]; then
   echo "No submodule update to commit."
 else
   git add ./src/cf-performance-tests
-  git config --global user.name "testname"
-  git config --global user.email "test@sap.com"
+  git config --global user.name "$GIT_COMMIT_USERNAME"
+  git config --global user.email "$GIT_COMMIT_EMAIL"
   git commit -m "$commit_message"
-  echo "Updated submodule to $commit_commit_message"
+  echo "Updated submodule to $commit_message"
 fi
